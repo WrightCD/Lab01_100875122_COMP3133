e8c6b45436f2e15d6fe7fcc37ceed428b708246f
WrightCD <carldonovanwright@gmail.com>
2024-01-21 18:17:25 -0500

    Initial

---
+++ index.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const csv = require("csv-parser");
+
+function countryToFile(country, file) {
+  const writeStream = fs.createWriteStream(file);
+
+  writeStream.write(`country,year,population\n`);
+
+  fs.createReadStream("input_countries.csv")
+    .pipe(csv())
+    .on("data", (row) => {
+      if (row.country.toLowerCase() === country.toLowerCase()) {
+        writeStream.write(`${row.country},${row.year},${row.population}\n`);
+      }
+    });
+}
+
+countryToFile("Canada", "canada.txt");
+countryToFile("United States", "usa.txt");
